5c4dcad592538d4d20b99229b5d3f28e6b64b959
captainQ <lordkuvin@gmail.com>
2022-01-07 23:26:28 +0600

    Добавлена страница детального просмотра

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views import index_view
+from webapp.views import index_view, product_view
 
 urlpatterns = [
-    path('', index_view, name="index_view")
+    path('', index_view, name="index_view"),
+    path('product/<int:pk>/', product_view, name='product_view')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from webapp.models import Product
 
@@ -6,3 +6,9 @@ from webapp.models import Product
 def index_view(request):
     products = Product.objects.order_by('category')
     return render(request, 'index_view.html', {'products': products})
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    return render(request, 'product_view.html', {'product': product})
