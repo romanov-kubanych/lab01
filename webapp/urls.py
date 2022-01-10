cf9aa5f9213e343d5c549b6dcf91848145391e41
captainQ <lordkuvin@gmail.com>
2022-01-10 11:33:11 +0600

    Реализовано редактирование записей

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from webapp.views import index_view, product_view, create_view
+from webapp.views import index_view, product_view, create_view, update_view
 
 urlpatterns = [
     path('', index_view, name="index_view"),
     path('product/<int:pk>/', product_view, name='product_view'),
-    path('product/add/', create_view, name='create_view')
+    path('product/add/', create_view, name='create_view'),
+    path('product/<int:pk>/update', update_view, name='update_view')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -33,3 +33,27 @@ def create_view(request):
                                               price=price)
             return redirect('product_view', pk=new_task.pk)
         return render(request, 'product_create.html', {"form": form})
+
+
+def update_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        form = ProductForm(initial={
+            'title': product.title,
+            'description': product.description,
+            'category': product.category,
+            'balance': product.balance,
+            'price': product.price
+        })
+        return render(request, 'product_update.html', {'form': form, 'product': product})
+    else:
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            product.title = form.cleaned_data.get('title')
+            product.description = form.cleaned_data.get('description')
+            product.category = form.cleaned_data.get('category')
+            product.balance = form.cleaned_data.get('balance')
+            product.price = form.cleaned_data.get('price')
+            product.save()
+            return redirect('product_view', pk=product.pk)
+        return render(request, 'product_update.html', {'form': form, 'product': product})
